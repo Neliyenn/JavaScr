f1e761791856eae18b0592acf33f62215571a95a
Neliyenn <65779777+Neliyenn@users.noreply.github.com>
2020-10-13 14:06:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 //pobrabranie referencji
-const gallery = document.querySelectorAll('gallery.img');
+const gallery = document.querySelectorAll('.gallery img');
+
 
 //zapisanie sie na zdarzenie click
-
 for (let idx = 0; idx < gallery.length; idx++){
     const img = gallery[idx];
     img.addEventListener('click', showLightbox);
 }
 
 function showLightbox(ev){
-    console.log(ev.target);
+    console.dir(ev.target);
     const lightbox = document.querySelector('.lightbox');
     const img = document.querySelector('.lightbox img');
     const imgUrl = ev.target.src;
@@ -20,17 +20,17 @@ function showLightbox(ev){
 
 
 
-
-// zapisanie sie na zdarzenie click
-// firstImage.addEventListener('click'), showLightbox;
+// const firstImg = document.querySelector('#firstImg');
+// //zapisanie sie na zdarzenie click
+// firstImg.addEventListener('click'), showLightbox;
 
 // function showLightbox(){
 //     const lightbox = document.querySelector('.lightbox');
 //     lightbox.classList.add('visible');
 // }
 
-// powinno zamknac obrazek po kliknieciu w dowolne miejsce na ekranie
-// firstImage.addEventListener('click'), hideLightbox;
+// //powinno zamknac obrazek po kliknieciu w dowolne miejsce na ekranie
+// firstImg.addEventListener('click'), hideLightbox;
 
 // function hideLightbox(){
 //     const lightbox = document.querySelector('.lightbox');
