a864db4a90c3a0e94c7fffa26b93caecb382ca74
Neliyenn <65779777+Neliyenn@users.noreply.github.com>
2020-12-08 14:42:51 +0100

    /.

--- ball-in-hole/main.js
+++ ball-in-hole/main.js
@@ -1,24 +1,32 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 800;
+canvas.height = 700;
 
 class Ball {
     constructor(x, y, radius, color, velocity){
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
-        this.velocity = velocity;
+        this.x = 100;
+        this.y = 600;
+        this.radius = 20;
+        this.color = 'red';
     }
 
     draw(){
-        c.beginPath();
-        c.arc(this. x, this.y, this.radius, 0, Math.PI*2, false);
-        c.fillStyle = this.color;
-        c.fill();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+    
+    update(deltaTime){
+        if(deltaTime) return;
+
+        this.x += 5 / deltaTime;
+
     }
 }
 
@@ -30,21 +38,21 @@ class Hole {
         this.color = color;
     }
 
-    GetX()
-    {
-        return this.x;
-    }
+    // GetX()
+    // {
+    //     return this.x;
+    // }
 
-    GetY()
-    {
-        return this.y;
-    }
+    // GetY()
+    // {
+    //     return this.y;
+    // }
 
     draw(){
-        c.beginPath();
-        c.arc(this. x, this.y, this.radius, 0, Math.PI*2, false);
-        c.fillStyle = this.color;
-        c.fill();
+        ctx.beginPath();
+        ctx.arc(this. x, this.y, this.radius, 0, Math.PI*2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
     }
 }
 
@@ -57,18 +65,23 @@ class Finish {
     }
 
     draw(){
-        c.beginPath();
-        c.arc(this. x, this.y, this.radius, 0, Math.PI*2, false);
-        c.fillStyle = this.color;
-        c.fill();
+        ctx.beginPath();
+        ctx.arc(this. x, this.y, this.radius, 0, Math.PI*2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
     }
+
 }
 
-const ball = new Ball(100, Math.random()*800, 20, 'red');
-ball.draw();
+const ball = new Ball();
 
-const finish = new Finish(1600, Math.random()*900, 30, 'orange');
-finish.draw();
+let hole2 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
+let hole3 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
+let hole4 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
+let hole5 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
+let hole1 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
+
+const finish = new Finish(700, 100, 30, 'orange');
 
 // window.addEventListener('deviceorientation', onDeviceMove);
 
@@ -76,28 +89,52 @@ finish.draw();
 //     console.log(ev.alfa, ev.beta, ev.gamma);
 // }
 
-function setHoles() {
-    let i;
-    for(i = 0; i < 30; i++){
-        let cordX = 0;
-        let cordY = 0;
-        let checkX = true;
-        let checkY = true;
 
-        do
-        {
-            cordX = Math.random()*1400;
-            cordY = Math.random()*900;
+// function setHoles() {
+//     let i;
+//     for(i = 0; i < 30; i++){
+//         // let cordX = 0;
+//         // let cordY = 0;
+//         // let checkX = true;
+//         // let checkY = true;
+
+//         // do
+//         // {
+//         //     cordX = Math.random()*1400;
+//         //     cordY = Math.random()*900;
             
-            checkX = Math.abs(cordX - 100) < 50; 
-            checkY = Math.abs(cordY - Math.random()*800) < 50; 
-        }
-        while(checkX || checkY);
+//         //     checkX = Math.abs(cordX - 100) < 50; 
+//         //     checkY = Math.abs(cordY - Math.random()*800) < 50; 
+//         // }
+//         // while(checkX || checkY);
 
-        let hole = new Hole(cordX, cordY, 30, 'grey');
-        hole.draw();
-    }
+//            let hole = new Hole(Math.random() * 1400, Math.random() * 900, 30, 'grey');
+//         // hole.draw();
+//     }
 
+// }
+
+// setHoles();
+
+let lastTime = 0;
+
+function game(timestamp){
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball.update(deltaTime);
+    ball.draw();
+    hole1.draw();
+    hole2.draw();
+    hole3.draw();
+    hole4.draw();
+    hole5.draw();
+    finish.draw();
+
+    requestAnimationFrame(game);
 }
 
-setHoles();
+requestAnimationFrame(game);
+
+
