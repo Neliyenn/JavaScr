99baa8ca9c0a08678389dededc825d2edf1c7a4a
Neliyenn <65779777+Neliyenn@users.noreply.github.com>
2021-01-21 01:08:16 +0100

    Update main.js

--- ball-in-hole/main.js
+++ ball-in-hole/main.js
@@ -8,16 +8,13 @@ canvas.width = 800;
 canvas.height = 700;
 
 class Ball {
-    constructor(x, y, radius, color, speed){
+    constructor(x, y, radius, color){
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.color = color;
-        this.speed = speed;
-
-        // this.dx = ev.alpha;
-        // this.dy = ev.beta;
-      
+        this.dx = 0;
+        this.dy = 0;
     }
 
     draw(){
@@ -28,11 +25,9 @@ class Ball {
     }
     
     update(){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        this.draw(ctx);
         this.x += this.dx;
         this.y += this.dy;
-
+        this.draw(ctx);
     }
 }
 
@@ -69,7 +64,7 @@ class Finish {
 
 }
 
-let ball = new Ball(100, 600, 20, 'red', 5,);
+const ball = new Ball(100, 600, 20, 'red');
 ball.draw();
 
 let hole2 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
@@ -77,28 +72,60 @@ let hole3 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
 let hole4 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
 let hole5 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
 let hole1 = new Hole(Math.random() * 600, Math.random() * 500, 30, 'grey');
-hole1.draw();
-hole2.draw();
-hole3.draw();
-hole4.draw();
-hole5.draw();
 
 const finish = new Finish(700, 100, 30, 'orange');
-finish.draw();
+let distance = '';
+let distance1 = '';
+let distance2 = '';
+let distance3 = '';
+let distance4 = '';
+let distance5 = '';
 
 window.addEventListener('deviceorientation', onDeviceMove);
 
 function onDeviceMove(ev){
-    dx = ev.alpha/60;
-    dy = ev.beta/60;
+    ball.dx = ev.alpha/60;
+    ball.dy = ev.beta/60;
 }
 
 function game(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ball.update();
+    hole1.draw();
+    hole2.draw();
+    hole3.draw();
+    hole4.draw();
+    hole5.draw();
+    finish.draw();
+
+    distance = Math.hypot(ball.x - hole1.x, ball.y - hole1.y);
+    distance1 = Math.hypot(ball.x - hole2.x, ball.y - hole2.y);
+    distance2 = Math.hypot(ball.x - hole3.x, ball.y - hole3.y);
+    distance3 = Math.hypot(ball.x - hole4.x, ball.y - hole4.y);
+    distance4 = Math.hypot(ball.x - hole5.x, ball.y - hole5.y);
+    distance5 = Math.hypot(ball.x - finish.x, ball.y - finish.y);
+
+    if(distance - ball.radius - hole1.radius < 1){
+        alert('You Loose!');
+    }
+    else if(distance1 - ball.radius - hole2.radius < 1){
+        alert('You Loose!');
+    }
+    else if (distance2 - ball.radius - hole3.radius < 1){
+        alert('You Loose!');
+    }
+    else if (distance3 - ball.radius - hole4.radius < 1){
+        alert('You Loose!');
+    }
+    else if (distance4 - ball.radius - hole5.radius < 1){
+        alert('You Loose!');
+    }
+    else if (distance5 - ball.radius - finish.radius < 1){
+        alert('You Win!');
+    }
+    
     requestAnimationFrame(game);
 }
-
-onDeviceMove(ev);
-requestAnimationFrame(game);
+game();
 
 
